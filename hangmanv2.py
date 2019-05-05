745cf65a822034c769f3ff22b7ecba004cfce4f4
asekkha <asekkha.sinchai@gmail.com>
2019-05-05 19:47:18 +0700

    New file game v3
    turn code to object programming
    add private variable in class (bestleft,hangman)

--- hangmanv2.py
+++ hangmanv2.py
@@ -1,8 +1,8 @@
 import random
 
 class Game(object):
-    best_left = 0
-    hangman = [
+    _best_left = 0
+    _hangman = [
         """
             -----
             |   |
@@ -80,12 +80,12 @@ class Game(object):
         guesses_left = 6
         wrong_count = 0
         letter_storage = []
-        print(self.hangman[wrong_count] + "\n")
+        print(self._hangman[wrong_count] + "\n")
         while guesses_left > 0 and not dashes == secret_word:
 
             # Print the amount of dashes and guesses left
             print(dashes)
-            print("Best left: " + str(self.best_left))
+            print("Best left: " + str(self._best_left))
             print("Guess(es) left: " + str(guesses_left))
 
             # Ask for input
@@ -94,7 +94,7 @@ class Game(object):
             # Check if user guess the whole word    
             if guess == secret_word:
                 print("Congrats! You win. You just guessed the whole word!")
-                print(self.hangman[wrong_count] + "\n")
+                print(self._hangman[wrong_count] + "\n")
                 break
          
             #resetGame
@@ -104,12 +104,12 @@ class Game(object):
             # Invalid inputs
             elif len(guess) != 1:
                 print("Your guess must have exactly one character!")
-                print(self.hangman[wrong_count] + "\n")
+                print(self._hangman[wrong_count] + "\n")
         
             # input only alphabet     
             elif not guess.isalpha():
                 print("Your guess can only contains alphabet!")
-                print(self.hangman[wrong_count] + "\n")
+                print(self._hangman[wrong_count] + "\n")
 
             #checking if letter has been already used
             elif guess in letter_storage:
@@ -118,14 +118,14 @@ class Game(object):
             # the guess is in the secret word
             elif guess in secret_word:
                 print("That letter is in the secret word!")
-                print(self.hangman[wrong_count] + "\n")
+                print(self._hangman[wrong_count] + "\n")
                 dashes = Game.update_dashes(self,secret_word, dashes, guess)
 
             else:
                 wrong_count += 1
                 guesses_left -= 1
                 print("That letter is not in the secret word!\n")
-                print(self.hangman[wrong_count] + "\n")
+                print(self._hangman[wrong_count] + "\n")
 
             # add guessed letter to a list
             letter_storage.append(guess)
@@ -141,9 +141,9 @@ class Game(object):
         # User wins
         else:
             print("Congrats! You win. The word was: " + str(secret_word))
-            if self.best_left < guesses_left:
-                self.best_left = guesses_left
-                print("Congrats! You got the new best. The best left is: " + str(self.best_left))
+            if self._best_left < guesses_left:
+                self._best_left = guesses_left
+                print("Congrats! You got the new best. The best left is: " + str(self._best_left))
 
 WORD_FILE = 'words.txt'
 if __name__ == "__main__" :
@@ -155,7 +155,7 @@ if __name__ == "__main__" :
     while True:
         tmp = input("Play again ?[y/n]: ").lower()
         if tmp == "n":
-            print("Your best left is: " + str(Game.best_left))
+            print("Your best left is: " + str(Game._best_left))
             print("BYE")
             break
         elif tmp == "y":

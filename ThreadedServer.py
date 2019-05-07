1438390e37ef8bcd443028ce3b51fcd98186e256
Narawit <tumnarawit@gmail.com>
2019-05-08 01:02:41 +0700

    Reset game function
    can reset game in socket version

--- ThreadedServer.py
+++ ThreadedServer.py
@@ -17,30 +17,45 @@ class SingleTCPHandler(socketserver.BaseRequestHandler):
         name = str(self.request.recv(1024), 'utf-8') # get name from conn
         game = core_game_server.Game(name) # new game
         print("{} | Player name: {}".format(cur_thread.name ,game.player_name))
-        print("{} | Secret word is: {}".format(cur_thread.name ,game._secret_word))
-        #initial
-        self.request.sendall(bytes(game.dashes, 'utf-8'))
-        time.sleep(0.1)
-        # TODO: best score from db
-        self.request.sendall(bytes(str(game.guesses_left), 'utf-8'))
-        time.sleep(0.1)
-        letter_storage = json.dumps(game.letter_storage)
-        self.request.sendall(letter_storage.encode())
-        time.sleep(0.1)
-        #game loop
         while 1:
-            guess = str(self.request.recv(1024), 'utf-8') # get guess from conn
-            if not guess: break
-            print("{} | Guess: {}".format(cur_thread.name ,guess))
-            if guess == "reset":
+            print("{} | Secret word is: {}".format(cur_thread.name ,game._secret_word))
+            #initial
+            self.request.sendall(bytes(game.dashes, 'utf-8'))
+            time.sleep(0.1)
+            # TODO: best score from db
+            self.request.sendall(bytes(str(game.guesses_left), 'utf-8'))
+            time.sleep(0.1)
+            letter_storage = json.dumps(game.letter_storage)
+            self.request.sendall(letter_storage.encode())
+            time.sleep(0.1)
+            #game loop
+        
+            while game.guesses_left > 0 and not game.dashes == game._secret_word:
+                guess = str(self.request.recv(1024), 'utf-8') # get guess from conn
+                if not guess: break
+                print("{} | Guess: {}".format(cur_thread.name ,guess))
+                if guess == "reset":
+                    game.reset()
+                    continue
+                if game.check_letter(guess):
+                    self.request.sendall(bytes('1', 'utf-8')) # never guess this letter
+                    time.sleep(0.1)
+
+                    is_whole = game.check_guess(guess)
+                    print("{} | ans type: {}".format(cur_thread.name, is_whole))
+                    self.request.sendall(bytes(is_whole, 'utf-8')) # send type of guess
+                    time.sleep(0.1)
+                    if is_whole: break
+                else:
+                    self.request.sendall(bytes('0', 'utf-8'))
+                    time.sleep(0.1) #send back error message
+
+            self.request.sendall(bytes("rox", 'utf-8')) # reset or exit
+            end_ans = str(self.request.recv(1024), 'utf-8')
+            if end_ans == "reset":
                 game.reset()
                 continue
-            if game.check_letter(guess):
-                self.request.sendall(bytes('1', 'utf-8'))
-                time.sleep(0.1)
-            else:
-                self.request.sendall(bytes('0', 'utf-8'))
-                time.sleep(0.1) #send back error message
+            break
         # loop end
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
--- conn.py
+++ conn.py
@@ -28,14 +28,14 @@ if __name__ == "__main__":
     try:
         name = input("Please enter your name: ")
         usr.send_message(name)
-        game = core_game_client.Game() # hangman list from core_game_cli
-        print("\n   {}\n".format(usr.get_message())) # get dashes
-        guess_left = usr.get_message()
-        print("Guess(es) left: {}".format(guess_left))
-        data = usr.sock.recv(1024)
-        letter_storage = json.loads(data)
-        print("Used letters: {}".format(repr(letter_storage)))
         while 1:
+            game = core_game_client.Game() # hangman list from core_game_cli
+            print("\n   {}\n".format(usr.get_message())) # get dashes
+            guess_left = usr.get_message()
+            print("Guess(es) left: {}".format(guess_left))
+            data = usr.sock.recv(1024)
+            letter_storage = json.loads(data)
+            print("Used letters: {}".format(repr(letter_storage)))
             guess = input("Guess: ").lower()
             usr.send_message(guess) # get guess and send to server
             if guess == "reset":
@@ -43,6 +43,26 @@ if __name__ == "__main__":
             guessinserver = int(usr.get_message())
             # print(guessinserver)
             game.check_letter(guessinserver, guess) # send boolean if guess is in server to client
+            #check for answer type
+            ans_type = usr.get_message()
+            if ans_type == "we":
+                print("Congrats! You win. You just guessed the whole word and get extra score!")
+            elif ans_type == "w":
+                print("Congrats! You win. You just guessed the whole word! but did not get extra score :(")
+
+            #end game
+            if usr.get_message() == "rox":
+                while 1:
+                    again = input("Play again? [y/n]: ").lower()
+                    if not (again == "y" or again == "yes" or again == "n" or again == "no"):
+                        print("command '{}' not regcognize".format(again))
+                        continue
+                    break
+                if again == "y" or again == "yes":
+                    usr.send_message("reset")
+                    continue
+                break
+                
 
     except (OverflowError, IOError):
         print("somthings wrong...")

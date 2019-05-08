681021a26d6eac9700eb86b17c7c611b9698f2ae
Narawit <tumnarawit@gmail.com>
2019-05-08 11:08:44 +0700

    Fix check_letter function

--- core_game_client.py
+++ core_game_client.py
@@ -60,10 +60,13 @@ class Game():
             =========""")
 
      def check_letter(self, isinserver, guess):
-        if isinserver == 1 and len(guess) == 1:
+          if isinserver and len(guess) == 1:
             self.letter_storage.append(guess)
-        else:
-             print("Sorry, you're already guessed this letter!")
+          elif len(guess) == 1:
+               print("Sorry, you're already guessed this letter!")
+          else:
+               return 0
+
 
      def __init__(self):
           self.wrong_count = 0
@@ -83,6 +86,6 @@ class Game():
 
      def check_guess(self, is_in_secret, guess):
           if is_in_secret:
-               print("The letter {} is in the secret word!".format(guess))
+               print("The letter '{}' is in the secret word!".format(guess))
           else:
-               print("The letter {} is not in the secret word!".format(guess))
+               print("The letter '{}' is not in the secret word!".format(guess))
